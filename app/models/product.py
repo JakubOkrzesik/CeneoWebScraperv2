4136b53b319861fce15b48b053abf3141e4b8472
JakubOkrzesik <kukistockx@gmail.com>
2022-06-10 11:14:19 +0200

    Added new files and modified/fixed others

--- app/models/product.py
+++ app/models/product.py
@@ -1,6 +1,84 @@
-from pickletools import read_unicodestring1
+from app import app
+from flask import render_template, redirect, url_for, request
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+import json
+from app.models.opinion import Opinion
+from app.utils import get_item()
+
 
 
 class Product:
-    def __init__(self):
-        return
+    def __init__(self, product_id=0, opinions=[], product_name='', 
+    opinions_count=0, pros_count=0, cons_count=0, average_score=0, opinions_stars = 0):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.opinions = opinions
+        self.opinions_count = opinions_count
+        self.pros_count = pros_count
+        self.cons_count = cons_count
+        self.average_score = average_score
+        self.opinions_stars = opinions_stars
+        
+        return self
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def to_dict(self):
+        pass
+    
+    def extract_product(self):
+        url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+        all_opinions = []
+
+        while(url):
+            response = requests.get(url)
+
+            page = BeautifulSoup(response.text, 'html.parser')
+
+            opinions = page.select('div.js_product-review')
+
+            for opinion in opinions:
+                self.opinions.append(Opinion().extract_opinion(opinion))
+                
+            
+            try:
+                url = "https://www.ceneo.pl" + get_item(page,'a.pagination__next', 'href')
+            except TypeError:
+                url = None
+            
+    def process_stats(self):
+        opinions = pd.read_json(json.dumps(self.opinions))
+        self.opinions_count = len(self.opinions.index)
+        self.pros_count = self.opinions['pros'].map(bool).sum()
+        self.cons_count = self.opinions['cons'].map(bool).sum()
+        self.average_score = self.opinions['stars'].mean().round(2)
+        self.opinions_stars = self.opinions['stars'].map(lambda x: float(x.split("/")[0].replace(",", ".")))
+
+        return self
+    
+    
+    
+    def save_opinions(self):
+        if not os.path.exists("app/opinions"):
+                os.makedirs("app/opinions")
+
+
+        with open(f'app/opinions/{self.product_id}.json', 'w', encoding='UTF-8') as f:
+            json.dump(self.opinions, f, indent=4, ensure_ascii=False)
+
+    def save_stats(self):
+        if not os.path.exists("app/products"):
+                os.makedirs("app/products")
+
+
+        with open(f'app/products/{self.product_id}.json', 'a', encoding='UTF-8') as f:
+            json.dump(self.opinions, f, indent=4, ensure_ascii=False)
--- app/routes.py
+++ app/routes.py
@@ -7,37 +7,13 @@ from matplotlib import pyplot as plt
 import requests
 from bs4 import BeautifulSoup
 import json
+from app.models.product import Product
+from app.utils import get_item
 
 plt.switch_backend('Agg')
 
-def get_item(ancestor, selector, attribute=None, return_list = False):
-    try:
-        if return_list:
-            return [item.get_text().strip() for item in ancestor.select(selector)]
-        if attribute:
-            return ancestor.select_one(selector)[attribute]
-        else:
-            return ancestor.select_one(selector).get_text().strip()
-    except (AttributeError, TypeError):
-        return None
+
     
-selectors = {
-    'author' : ['span.user-post__author-name'],
-    'recommendation': ['span.user-post__author-recomendation > em'],
-    'stars' : ['span.user-post__score-count'],
-    "content": ["div.user-post__text"],
-    'useful' : ['button.vote-yes > span'],
-    'useless' : ['button.vote-no > span'],
-    'publish_date' : ['span.user-post__published > time:nth-child(2)', 'datetime'],
-    'purchase_date' : ['span.user-post__published > time:nth-child(2)', 'datetime'],
-    'pros' : ['div.review-feature__title--positives ~ div.review-feature__item', None, True],
-    'cons' : ['div.review-feature__title--negatives ~ div.review-feature__item', None, True]
-}
-
-
-
-
-
 @app.route('/')
 
 def index():
@@ -50,37 +26,9 @@ def extract():
     if request.method == 'POST':
 
         product_id = request.form.get("product_id")
-        url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
-        all_opinions = []
+        product = Product(product_id)
+        product.extract_product()
 
-        while(url):
-            response = requests.get(url)
-
-            page = BeautifulSoup(response.text, 'html.parser')
-
-            opinions = page.select('div.js_product-review')
-
-            for opinion in opinions:
-        
-                single_opinion = {
-                    key:get_item(opinion, *value)
-                        for key, value in selectors.items()
-                }
-                single_opinion["opinion_id"] = opinion["data-entry-id"]
-
-                all_opinions.append(single_opinion)
-            
-            try:
-                url = "https://www.ceneo.pl" + get_item(page,'a.pagination__next', 'href')
-            except TypeError:
-                url = None
-            
-            if not os.path.exists("app/opinions"):
-                os.makedirs("app/opinions")
-
-
-            with open(f'app/opinions/{product_id}.json', 'w', encoding='UTF-8') as f:
-                json.dump(all_opinions, f, indent=4, ensure_ascii=False)
         
         return redirect(url_for('product', product_id = product_id))
     else:
